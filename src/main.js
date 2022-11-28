8d4c00449b2b5e9efab24e8fdcd714751048d384
xmyzcauchsm <403331214@qq.com>
2022-11-28 15:32:26 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载全局样式
 import '@/styles/index.less'
-
+// 加载vant组件库
+import Vant from 'vant'
+// 加载vant样式
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
